9930c5cc522370d0b8c777621ec107219d08bba7
paras <mail2paras.s@gmail.com>
2018-03-17 21:17:50 +0530

    [UPDATE] UI

--- web-app/src/containers/Farmer.js
+++ web-app/src/containers/Farmer.js
@@ -1,12 +1,82 @@
 import React, { Component } from 'react';
 import Nav from '../components/Nav';
+import Web3 from 'web3';
+import { Card, Subheader } from 'react-md';
+import {
+  Button,
+  FontIcon,
+  SVGIcon,
+  TextField,
+} from 'react-md';
 
 class Farmer extends Component {
+  state = {
+    username: "",
+    requirements: "",
+    price: "2131",
+    eq_rs: "31331",
+    products: [],
+    transactions: []
+  }
 
+  requirement_submit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
-      <div>
+      <div style={{height: '100vh', background: 'linear-gradient(217deg, rgba(0,255,0,.8), rgba(255,0,0,0) 70.71%)'}}>
         <Nav/>
+        <Card style={{margin: '10px', maxWidth: '400px', margin: 'auto', marginTop: '20px', textAlign: 'center', padding: '10px'}}>
+          <b>{this.state.price}</b> Aadhar Coin = <b>Rs. {this.state.eq_rs}</b>
+        </Card>
+        <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+          <Card style={{margin: '10px',minWidth: '300px', marginTop: '20px', padding: '10px'}}>
+          <h3>Add Requirement</h3>
+          <form onSubmit={(e) => this.requirement_submit(e)}>
+            <TextField
+              id="phone-number"
+              label="Phone"
+              fullWidth
+            />
+            <TextField
+              id="aadhar"
+              label="Aadhar Number"
+              fullWidth
+            />
+            <TextField
+              id="capital"
+              label="Capital to be raised"
+              fullWidth
+            />
+            <TextField
+              id="time"
+              label="Time Duration"
+              fullWidth
+            />
+            <TextField
+              id="type"
+              label="Crop Name | Type"
+              fullWidth
+            />
+            <div className="md-text-center" style={{marginTop: '20px'}}>
+              <Button 
+                type="submit" 
+                raised 
+                primary>
+                Request
+              </Button>
+            </div>
+            </form>
+          </Card>
+          <Card style={{margin: '10px',minWidth: '300px', marginTop: '20px', padding: '10px'}}>
+          <h3>Transation History</h3>
+          <p hidden={!this.state.transactions.length === 0}>No Transaction History...</p>
+          </Card>
+          <Card style={{margin: '10px',minWidth: '300px', marginTop: '20px', padding: '10px'}}>
+            <h3>Agro Products</h3>
+            <div hidden={!this.state.products.length === 0}>No Products available...</div>
+          </Card>
+        </div>
       </div>
     );
   }
--- web-app/src/containers/Investor.js
+++ web-app/src/containers/Investor.js
@@ -1,16 +1,84 @@
 import React, { Component } from 'react';
 import Nav from '../components/Nav';
+import Web3 from 'web3';
+import { Card, Subheader } from 'react-md';
+import {
+  Button,
+  FontIcon,
+  SVGIcon,
+  TextField,
+} from 'react-md';
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemControl,
+  Checkbox,
+  Switch
+} from 'react-md';
+import ChartistGraph from 'react-chartist';
 
-class Investor extends Component {
+const CLASS_NAME = 'md-cell md-cell--6 md-paper md-paper--1';
+var biPolarBarChartData = {
+  labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
+  series: [
+    [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+  ]
+};
+var biPolarBarChartOptions = {
+  high: 10,
+  low: -10,
+  axisX: {
+    labelInterpolationFnc: function(value, index) {
+      return index % 2 === 0 ? value : null;
+    }
+  }
+}
+class Investors extends Component {
+  state = {
+    username: "",
+    price: "2131",
+    eq_rs: "31331",
+    crops: [1,2,3],
+    transactions: []
+  }
 
+  requirement_submit(e) {
+    e.preventDefault();
+  }
   render() {
     return (
-      <div>
+      <div style={{height: '100vh', background: 'linear-gradient(217deg, rgba(0,255,0,.8), rgba(255,0,0,0) 70.71%)'}}>
         <Nav/>
+        <Card style={{margin: '10px', maxWidth: '400px', margin: 'auto', marginTop: '20px', textAlign: 'center', padding: '10px'}}>
+          <ChartistGraph data={biPolarBarChartData} options={biPolarBarChartOptions} type={'Line'} />
+          <b>{this.state.price}</b> Krishi Mudra = <b>Rs. {this.state.eq_rs}</b>
+        </Card>
+        <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+          <Card style={{margin: '10px',minWidth: '300px', marginTop: '20px', padding: '10px'}}>
+            <h3>Open Fundings</h3>
+            <List>
+              {this.state.crops.map((e, i) => {
+                  <ListItemControl
+                  leftAvatar={<Avatar suffix="teal">{e.name}</Avatar>}
+                  secondaryAction={
+                    <Button raised primary>
+                      Fund
+                    </Button>
+                  }/>
+              })}
+            </List>
+            <div hidden={!this.state.crops.length === 0}>No open fundings...</div>
+          </Card>
+          <Card style={{margin: '10px',minWidth: '300px', marginTop: '20px', padding: '10px'}}>
+          <h3>Transation History</h3>
+          <div hidden={!this.state.transactions.length === 0}>No Transaction History...</div>
+          </Card>
+        </div>
       </div>
     );
   }
 }
 
 
-export default Investor;
+export default Investors;
